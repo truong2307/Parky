cba0795cfab1f248f56007998502b69544e9d945
Truong10Ngon <toiiuvn013@gmail.com>
2021-10-18 20:40:25 +0700

    Trail, national park repository

---
+++ ParkyWeb/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,13 @@
+﻿using ParkyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    public interface INationalParkRepository : IRepository<NationalPark>
+    {
+
+    }
+}
--- ParkyWeb/Startup.cs
+++ ParkyWeb/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ParkyWeb.Repository;
+using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,8 @@ namespace ParkyWeb
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddHttpClient();
+            services.AddScoped<INationalParkRepository, NationalParkRepository>();
+            services.AddScoped<ITrailRepository, TrailRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
