021529c8c98a8915ffa503b3a5361d16fb70b686
Truong10Ngon <toiiuvn013@gmail.com>
2021-10-20 22:01:10 +0700

    display trail

---
+++ ParkyWeb/wwwroot/js/Trail.js
@@ -0,0 +1,59 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Trails/GetAllTrails",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "nationalPark.name", "width": "20%" },
+            { "data": "name", "width": "20%" },
+            { "data": "distance", "width": "15%" },
+            { "data": "elevation", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Trails/Upsert/${data}" class='btn btn-success text-white'
+                                    style='cursor:pointer;'> <i class='far fa-edit'></i></a>
+                                    &nbsp;
+                                <a onclick=Delete("/Trails/DeleteTrail/${data}") class='btn btn-danger text-white'
+                                    style='cursor:pointer;'> <i class='far fa-trash-alt'></i></a>
+                                </div>`;
+                }
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Bạn có muốn xóa không?",
+        text: "Xóa sẽ không khôi phục lại dữ liệu, bạn có chắc chắn?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
