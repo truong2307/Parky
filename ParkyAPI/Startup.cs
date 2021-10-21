1887c611411b1c80a0683c44b30b941332226606
Truong10Ngon <toiiuvn013@gmail.com>
2021-10-22 02:18:08 +0700

    add cors

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -39,7 +39,7 @@ namespace ParkyAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -144,6 +144,11 @@ namespace ParkyAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 
             app.UseAuthentication();
 
